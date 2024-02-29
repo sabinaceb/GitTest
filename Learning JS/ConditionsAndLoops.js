d907bdcb7de807f089cd5941b13c57c9f45f666b
Sabina Cebreroz Silva <sabinaceb@gmail.com>
2024-02-28 21:45:26 -0600

    Task: Variables and Data Types

---
+++ Learning JS/ConditionsAndLoops.js
@@ -0,0 +1,15 @@
+//INTRO
+if(true) {
+    // this block of code will be executed
+}
+
+if(false) {
+// this block of code will NOT be executed
+}
+
+//IF... ELSE
+if (2 * 4 == 8) {
+    console.log('That is right!');
+  } else {
+    console.log('Wrong!');
+  }
