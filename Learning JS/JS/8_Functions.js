8e8edbfc23d72362d4830d6a2e0f4d8007827597
Sabina Cebreroz Silva <sabinaceb@gmail.com>
2024-03-06 01:30:02 -0600

    Functions

--- Learning JS/JS/8_Functions.js
+++ Learning JS/JS/8_Functions.js
@@ -1,68 +1,149 @@
-// Example (hoisting)
-showMessage('Hello World!!!');
-function showMessage(text) {
-  console.log(text);
-}
+/**
+ * Learning materials:
+ * Function declaration: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/function
+ * Function expression: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/function
+ * Arrow functions: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+ */
 
-// Example (let and scope)
-let num = 10;
-function showMessage(text) { //se sobre-escribe la funcion, entre mas abajo mayor prioridad
-  let num = 1;
-  console.log(num);
-  console.log(text);
-}
-
-showMessage('hello!!'); // 1
-console.log(num); // 10
-
-// Example
-function calc(a, b) {
+/**
+ * write function that will do sum for two numbers
+ * @example
+ * console.log(sum(1, 3)) //4
+ * console.log(sum(-1, 1)) //0
+ */
+function sum(a, b) {
   return a + b;
 }
-console.log(calc(2, 5)); // 7
-console.log(calc(3, 2)); // 5
 
-// Example with Hosisting
-console.log(calc2(2,6));
-const calc2 = function(a, b) {
-    return a + b;
+/**
+* write function that returns firstName and lastName of a given object that contains firstName and lastName
+* @example
+* const person = {
+*    firstName: "John",
+*    lastName: "Dou"
+* }
+* console.log(getFullName(person)) // John Dou
+*/
+function getFullName(person) {
+  return person.firstName + " " + person.lastName; 
 }
 
-
-// Example
-function returnValue() {
-  let num = 50;
-  return num;
+/**
+* write function that checks if number is odd
+* true if odd, false if even
+* @example
+* console.log(isOdd(2)) // false
+* console.log(isOdd(3)) // true
+* hint: try using Remainder: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Remainder operator
+*/
+function isOdd(n) {
+  if (n%2 == 0) {
+      return false;
+  } else {
+      return true;
+  }
 }
 
-let anotherNum = returnValue();
-console.log(anotherNum); // 50
-
-
-
-
-//############################################
-// Example of Function Expression
-const calc = function(a, b) {
-    return (a + b);
+/**
+* write function that returns shortest of the words in the given array
+* @example
+* console.log(getShortest(["one", "three", "four"])) // one
+*/
+function getShortest(wordArray) {
+  let shortestWord = wordArray[0];
+  for (let word in wordArray) {
+      if (wordArray[word].length < shortestWord.length) {
+          shortestWord = wordArray[word];
+      }
+  }
+  return shortestWord;
 }
-console.log(calc(2,5));
 
+/**
+* write function that returns word google with given numbers of "o" symbols
+* @example
+* console.log(getGoogle(5)) // gooooogle
+*/
+function getGoogle(n) {
+  let str ="";
+  for(let i=0; i<n; i++) {
+      str += "o";
+  }
+  return "g" + str + "gle"
+}
 
+/**
+* write function that returns object based on the given information
+* (params may be null, so, please use default ones)
+* @example
+* getUser("John", "Dou", 42) should return
+* {
+*    firstName: "John",
+*    lastName: "Dou",
+*    age: 42
+* }
+*/
+function getUser(firstName=null, lastName=null, age=null) {
+  let obj = {
+      firstName: firstName,
+      lastName: lastName,
+      age: age
+  }
+  return obj;
+}
 
-//############################################
-// Example of IIFE
-(function (){
-    console.log('Hello World!')
-})() // Output: Hello World!
+/**
+* write function that calculates total path traveled.
+* path represented as array of objects with field distance and direction
+* e.g [{direction: "Kiyv - Minsk", distance: 567}, {direction: "Kiyv - Paris", distance: 2402}]
+* @example
+* const travel = [{direction: "Kiyv - Minsk", distance: 567}, {direction: "Kiyv - Paris", distance: 2402}];
+* console.log(getTotalPath(travel)) //2969
+*/
+function getTotalPath(path) {
+  let total = 0;
+  for(let idx in path){
+      total += path[idx].distance;
+  }
+  return total;
+}
 
-// Example of IIFE with parameters
-const myFunc = (function (name){
-    return 'Hello ' + name
-})('World!')
-console.log(myFunc); // Output: Hello World!
+/**
+* write a function that calculates a final price considering the Amount
+* reduced by discount percentage(hint: you need to use the Closure here)
+* JS Closures: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
+* @example
+* const discount10 = discountFunction(10);
+* console.log(discount10(90)); // 81
+* console.log(discount10(100)); // 90
+*/
 
-//Ex. 2
-(function (name){
-    console.log(`Hello ${name}`);
-})('Jack');
+function discountFunction(percentage) {
+  return function (amount) {
+      return amount - (amount * percentage) / 100;
+  };
+}
+const func = discountFunction(10);
+
+/**
+* Write the methods inside the given objects that:
+* 1. console logs keys of the given object (please use for..in cycle)
+* 2. returns the string 'My name is John Doe, and I am 25 years old. My best friend is Daniel'
+* referring to the data stored in the object. The string should be constructed using the properties from the object
+*/
+const myObject = {
+  name: 'John',
+  lastName: 'Doe',
+  age: 25,
+  friends: ['Mike', 'Alan', 'Daniel'],
+  keys() {
+      //write your code here
+      for (let key in myObject) {
+          console.log(key)
+      }
+  },
+  call() {
+      //write your code here
+      return `My name is ${this.name} ${this.lastName}, and I am ${this.age} years old. My best friend is ${this.friends[2]}`;
+  },
+};
